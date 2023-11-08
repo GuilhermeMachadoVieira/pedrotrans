974c4d1706fad7f0e635affa643d9c36cb2e3b6f
GuilhermeMachadoVieira <guilherme.vieira25@fatec.sp.gov.br>
2023-11-08 01:36:34 -0300

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+        <div className='main'>
+          <div className='center'>
+    <div className='menu'>
+    <div className='logo'>
+    <h3 className='trans'>TRANS</h3>
+    <h3 className='pedro'>PEDRO</h3>
+    </div>
+    <div className='item-menu'>
+      <a href='#'>Login</a>
+    </div>
+    </div>
+        </div>
+    </div>
+
+    <div className='primeiro-container'>
+    <h3>Aqui, entregamos excelencia, onde sua satisfação é o nosso destino.</h3>
+    </div>
+
+    
+    </div>
   )
 }
 
